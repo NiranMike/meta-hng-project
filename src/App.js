a0678998325f02d0a342d8d96dc0458792706ef3
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-11-11 12:06:36 +0100

    style: make inspiration section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import SponsorSection from './Components/SponsorSection'
+import InspirationSection from './Components/InspirationSection'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <SponsorSection />
+      <InspirationSection />
     </div>
   )
 }
