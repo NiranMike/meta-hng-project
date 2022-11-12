493398e938f54cf3446defa58db53e9703e3883b
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-11-12 11:27:23 +0100

    add pages folder

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import SponsorSection from '../Components/SponsorSection'
+import InspirationSection from '../Components/InspirationSection'
+import DiscoverSection from '../Components/DiscoverSection'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <SponsorSection />
+      <InspirationSection />
+      <DiscoverSection />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Pages/PlaceToStay.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PlaceList from '../Components/PlaceList';
+import PlaceNav from '../Components/PlaceNav';
+const PlaceToStay = () => {
+  return (
+    <>
+        <PlaceNav />
+        <PlaceList />
+    </>
+  );
+}
+
+export default PlaceToStay
